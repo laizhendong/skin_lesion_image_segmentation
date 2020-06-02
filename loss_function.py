9e0da6b530e52e269f73320204f2043229407215
laizhendong <zhendong_lai0621@163.com>
2020-06-02 19:43:26 +0800

    loss_function.py提交

---
+++ loss_function.py
@@ -0,0 +1,146 @@
+import keras
+import tensorflow as tf
+from keras.models import Model
+from keras import backend as K
+from keras.layers import Input, merge, Conv2D, ZeroPadding2D, UpSampling2D, Dense, concatenate, Conv2DTranspose
+from keras.layers.pooling import MaxPooling2D, GlobalAveragePooling2D, MaxPooling2D
+from keras.layers.core import Dense, Dropout, Activation
+from keras.layers import BatchNormalization, Dropout, Flatten, Lambda
+from keras.layers.advanced_activations import ELU, LeakyReLU
+from keras.optimizers import Adam, RMSprop, SGD
+from keras.regularizers import l2
+from keras.layers.noise import GaussianDropout
+import skimage.io as io
+import numpy as np
+import skimage.transform as trans
+import os
+import glob
+from keras.losses import binary_crossentropy
+import pydensecrf.densecrf as dcrf
+
+epsilon = 1e-6
+smooth = 1
+
+def confusion(y_true, y_pred):
+    smooth=1
+    y_pred_pos = K.clip(y_pred, 0, 1)
+    y_pred_neg = 1 - y_pred_pos
+    y_pos = K.clip(y_true, 0, 1)
+    y_neg = 1 - y_pos
+    tp = K.sum(y_pos * y_pred_pos)
+    fp = K.sum(y_neg * y_pred_pos)
+    fn = K.sum(y_pos * y_pred_neg) 
+    prec = (tp + smooth)/(tp+fp+smooth)
+    recall = (tp+smooth)/(tp+fn+smooth)
+    return prec, recall
+
+def tp(y_true, y_pred):
+    smooth = 1
+    y_pred_pos = K.round(K.clip(y_pred, 0, 1))
+    y_pos = K.round(K.clip(y_true, 0, 1))
+    tp = (K.sum(y_pos * y_pred_pos) + smooth)/ (K.sum(y_pos) + smooth) 
+    return tp 
+
+def tn(y_true, y_pred):
+    smooth = 1
+    y_pred_pos = K.round(K.clip(y_pred, 0, 1))
+    y_pred_neg = 1 - y_pred_pos
+    y_pos = K.round(K.clip(y_true, 0, 1))
+    y_neg = 1 - y_pos 
+    tn = (K.sum(y_neg * y_pred_neg) + smooth) / (K.sum(y_neg) + smooth )
+    return tn 
+
+def auc(y_true, y_pred):
+    smooth = 1
+    y_pred_pos = np.round(np.clip(y_pred, 0, 1))
+    y_pred_neg = 1 - y_pred_pos
+    y_pos = np.round(np.clip(y_true, 0, 1))
+    y_neg = 1 - y_pos
+    tp = np.sum(y_pos * y_pred_pos)
+    tn = np.sum(y_neg * y_pred_neg)
+    fp = np.sum(y_neg * y_pred_pos)
+    fn = np.sum(y_pos * y_pred_neg)
+    tpr = (tp + smooth) / (tp + fn + smooth) #recall
+    tnr = (tn + smooth) / (tn + fp + smooth)
+    prec = (tp + smooth) / (tp + fp + smooth) #precision
+    acc = (tp + tn + smooth) / (tp + tn + fn + fp + smooth)
+    return [tpr, tnr, prec, acc]
+
+# Custom loss function
+def dice_coef(y_true, y_pred):
+    smooth = 1.
+    y_true_f = K.flatten(y_true)
+    y_pred_f = K.flatten(y_pred)
+    intersection = K.sum(y_true_f * y_pred_f)
+    return (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
+
+def dice_coef_loss(y_true, y_pred):
+    return 1. - dice_coef(y_true, y_pred)
+
+#BCE + dice loss
+def bce_dice_loss(y_true, y_pred):
+    return binary_crossentropy(y_true, y_pred) + dice_coef(y_true, y_pred)
+
+#focal损失函数定义
+def focal_loss(gamma=2., alpha=.25):
+    def focal_loss_fixed(y_true, y_pred):
+        pt_1 = tf.where(tf.equal(y_true, 1), y_pred, tf.ones_like(y_pred))
+        pt_0 = tf.where(tf.equal(y_true, 0), y_pred, tf.zeros_like(y_pred))
+        return -K.sum(alpha * K.pow(1. - pt_1, gamma) * K.log(K.epsilon()+pt_1))-K.sum((1-alpha) * K.pow( pt_0, gamma) * K.log(1. - pt_0 + K.epsilon()))
+    return focal_loss_fixed
+
+#JACCARD损失函数定义
+def jaccard_coef(y_true, y_pred):
+    smooth = 1.
+    y_true_f = K.flatten(y_true)
+    y_pred_f = K.flatten(y_pred)
+    intersection = K.sum(y_true_f * y_pred_f)
+    return (intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) - intersection + smooth)
+
+def jaccard_coef_loss(y_true, y_pred):
+    return 1. - jaccard_coef(y_true, y_pred)
+
+#IOU loss
+def IoU_loss(y_true, y_pred, eps=1e-6):
+    if np.max(y_true) == 0.0:
+        return IoU(1-y_true, 1-y_pred) ## empty image; calc IoU of zeros
+    intersection = K.sum(y_true * y_pred, axis=[1,2,3])
+    union = K.sum(y_true, axis=[1,2,3]) + K.sum(y_pred, axis=[1,2,3]) - intersection
+    return -K.mean( (intersection + eps) / (union + eps), axis=0)
+
+#定义Tversky loss
+def tversky(y_true, y_pred):
+    y_true_pos = K.flatten(y_true)
+    y_pred_pos = K.flatten(y_pred)
+    true_pos = K.sum(y_true_pos * y_pred_pos)
+    false_neg = K.sum(y_true_pos * (1-y_pred_pos))
+    false_pos = K.sum((1-y_true_pos)*y_pred_pos)
+    alpha = 0.7
+    return (true_pos + smooth)/(true_pos + alpha*false_neg + (1-alpha)*false_pos + smooth)
+
+def tversky_loss(y_true, y_pred):
+    return 1 - tversky(y_true,y_pred)
+
+def focal_tversky(y_true,y_pred):
+    pt_1 = tversky(y_true, y_pred)
+    gamma = 0.75
+    return K.pow((1-pt_1), gamma)
+
+#GDL定义
+def generalized_dice_coeff(y_true, y_pred):
+    Ncl = y_pred.shape[-1]
+    w = K.zeros(shape=(Ncl,))
+    w = K.sum(y_true, axis=(0,1,2))
+    w = 1/(w**2+0.000001)
+    # Compute gen dice coef:
+    numerator = y_true*y_pred
+    numerator = w*K.sum(numerator,(0,1,2,3))
+    numerator = K.sum(numerator)
+    denominator = y_true+y_pred
+    denominator = w*K.sum(denominator,(0,1,2,3))
+    denominator = K.sum(denominator)
+    gen_dice_coef = 2*numerator/denominator
+    return gen_dice_coef
+
+def generalized_dice_loss(y_true, y_pred):
+    return 1 - generalized_dice_coeff(y_true, y_pred)
